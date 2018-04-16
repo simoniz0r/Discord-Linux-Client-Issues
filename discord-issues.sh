b4a1258db9fdd2ffb02a94ffb8aa423d073f3784
simoniz0r <bnu.fantasma@gmail.com>
2018-04-16 16:58:50 -0500

    Add script to output and search issues on Discord linux-issues Trello

---
+++ discord-issues.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Author: simonizor
+# Title: discord-issues
+# Description: Simple script to search the Discord Trello linux-issues board using curl and jq
+# License: MIT
+
+# download issues to /tmp/discord-linux-issuses.json to avoid multiple checks
+function getissues() {
+    rm -f /tmp/discord-linux-issues.json
+    curl -sSL -o /tmp/discord-linux-issues.json "https://api.trello.com/1/boards/UyU76Esh/cards" || { echo "Failed to download issues from Trello; exiting..."; exit 1; }
+}
+# use jq to output all issues in a readable format
+function showissues() {
+    START_ISSUE=0
+    for issue in $(jq -r '.[].shortUrl' /tmp/discord-linux-issues.json); do
+        echo -e "Name:\n$(jq -r ".[$START_ISSUE].name" /tmp/discord-linux-issues.json)"
+        echo -e "Last Date of Activity:\n$(jq -r ".[$START_ISSUE].dateLastActivity" /tmp/discord-linux-issues.json)"
+        echo -e "Description:\n$(jq -r ".[$START_ISSUE].desc" /tmp/discord-linux-issues.json)"
+        echo -e "Labels:\n$(jq -r ".[$START_ISSUE].labels" /tmp/discord-linux-issues.json)"
+        echo -e "URL:\n$(jq -r ".[$START_ISSUE].shortUrl" /tmp/discord-linux-issues.json)"
+        START_ISSUE=$(($START_ISSUE+1))
+        echo
+    done
+}
+# run a grep -qi containing the search input on the name of the issue
+function searchissues() {
+    echo "Searching for $@ ..."
+    START_ISSUE=0
+    for issue in $(jq -r '.[].shortUrl' /tmp/discord-linux-issues.json); do
+        if jq -r ".[$START_ISSUE].name" /tmp/discord-linux-issues.json | grep -qi "$@"; then
+            echo -e "URL:\n$(jq -r ".[$START_ISSUE].shortUrl" /tmp/discord-linux-issues.json)"
+            echo -e "Labels:\n$(jq -r ".[$START_ISSUE].labels" /tmp/discord-linux-issues.json)"
+            echo -e "Description:\n$(jq -r ".[$START_ISSUE].desc" /tmp/discord-linux-issues.json)"
+            echo -e "Last Date of Activity:\n$(jq -r ".[$START_ISSUE].dateLastActivity" /tmp/discord-linux-issues.json)"
+            echo -e "Name:\n$(jq -r ".[$START_ISSUE].name" /tmp/discord-linux-issues.json)"
+        fi
+        START_ISSUE=$(($START_ISSUE+1))
+    done
+}
+# check to see if jq is installed
+if ! type jq > /dev/null 2>&1; then
+    echo "jq is not installed; exiting..."
+    exit 1
+fi
+# detect arguments
+case "$1" in
+    -h|--help)
+        echo "A simple script that outputs and searches https://api.trello.com/1/boards/UyU76Esh/cards using curl and jq"
+        echo "Usage: discord-issues [argument] [search term(s)]"
+        echo
+        echo "Arguments:"
+        echo "-h, --help        Show this help output and exit."
+        echo
+        echo "-s, --search      Search for term(s) matching the input. '\|' can be used to search for multiple terms at once."
+        echo "                  Ex: discord-issues -s 'start\|KDE'"
+        echo
+        echo "If no argument is passed, the entire list of issues will be shown."
+        echo
+        exit 0
+        ;;
+    -s|--search)
+        getissues
+        searchissues "$@"
+        ;;
+    *)
+        getissues
+        showissues
+        ;;
+esac
+rm -f /tmp/discord-linux-issues.json
+exit 0
